38e9796f8df4da39b9b3b3386e944e84b714db19
Baran Akhavan - Yekta <200529109@student.georgianc.on.ca>
2023-11-15 17:21:38 -0500

    Assignment 3 Pizza Order completed

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javaassignment3200529109 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javaassignment3200529109 to javafx.fxml;
-    exports com.example.javaassignment3200529109;
-}
